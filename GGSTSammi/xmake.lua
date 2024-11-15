336f9bad7ce6c778f2c1a826b4fc63a19e0e916d
Dan <118778758+TheLettuceClub@users.noreply.github.com>
2024-11-15 17:47:54 -0500

    changed with no effect

--- GGSTSammi/xmake.lua
+++ GGSTSammi/xmake.lua
@@ -16,22 +16,7 @@ add_requires("cpr")
 target(projectName)
     add_packages("cpr")
 
-    after_load(function(target)
-        -- Binary outputs are written to the `Binaries` dir.
-        target:set("targetdir", path.join(os.projectdir(), "Binaries", get_config("mode"), target:name()))
-
-        target:set("kind", "shared")
-        target:set("languages", "cxx23")
-        target:set("exceptions", "cxx")
-        target:add("deps", "UE4SS")
-        target:set("group", "mods")
-    end)
-
-    on_install(function(target)
-        import("mods.install").install(target)
-    end)
-
-    -- add_rules("ue4ss.mod")
+    add_rules("ue4ss.mod")
 
     add_includedirs("include", { public = true })
     add_headerfiles("include/**.hpp")
